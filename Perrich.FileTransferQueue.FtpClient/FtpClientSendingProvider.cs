542488dfa229659d9bbaa8b35c730558a29af05c
perrich <github@perrichot.com>
2015-01-26 22:03:49 +0100

    Add System.Net.FtpClient implementation

---
+++ Perrich.FileTransferQueue.FtpClient/FtpClientSendingProvider.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+
+namespace Perrich.FileTransferQueue.FtpClient
+{
+    public class FtpClientSendingProvider : ISendingProvider
+    {
+        private readonly System.Net.FtpClient.FtpClient ftpClient;
+
+        public FtpClientSendingProvider(System.Net.FtpClient.FtpClient ftpClient)
+        {
+            this.ftpClient = ftpClient;
+        }
+
+        public bool Send(Stream srcStream, string destPath)
+        {
+            try
+            {
+                using (var s = ftpClient.OpenWrite(destPath))
+                {
+                    try
+                    {
+                        srcStream.CopyTo(s);
+                    }
+                    finally
+                    {
+                        s.Close();
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
